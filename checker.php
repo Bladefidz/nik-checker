e439e4da0e589c8d345aaaea2988cb8063720939
Hafidz <gamerhacking@gmail.com>
2015-10-11 05:50:45 +0700

    One Night Coding !

---
+++ checker.php
@@ -0,0 +1,23 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Goutte\Client;
+
+$client = new Client();
+
+$css = "#theForm > div.fboxbody > div.form > span";
+$baseURL = "http://data.kpu.go.id/ss8.php?cmd=cari&nik=";
+$nik = '3520082506930001';
+
+$resultArr = array();
+$crawler = $client->request('GET', $baseURL.$nik);
+$raw = $crawler
+	->filter($css)
+	->extract(array("_text"));
+
+for ($i=0; $i <= count($raw)-2; ) { 
+	$resultArr[$raw[$i]] = $raw[$i+1];
+	$i += 2;
+}
+
+print_r($resultArr);
